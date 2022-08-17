d4281c22f48cd94c251d175db97ec22d940b0a80
unknown <bbaska837@gmail.com>
2022-08-17 21:21:30 +0800

    new schema

---
+++ schemas/user.js
@@ -0,0 +1,13 @@
+export default {
+  name: "user",
+  type: "document",
+  title: "User",
+  fields: [
+    {
+      name: "name",
+      type: "string",
+      title: "Name",
+    },
+    { name: "password", type: "string", title: "Password" },
+  ],
+};
